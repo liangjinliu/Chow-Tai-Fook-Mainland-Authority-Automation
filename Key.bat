75cf06483cdc9b67f11a687d21dcb63ca154b0f5
liangjinliu <wjllnh@163.com>
2023-05-22 15:34:02 +0800

    add Chow-Tai-Fook-Mainland-Authority-Automation

---
+++ Key.bat
@@ -0,0 +1,21 @@
+@echo CC-port account name and password
+
+cmdkey /generic:SZ_OPS_Credentials /user:SYSRPA /pass:ctfops2024
+
+cmdkey /generic:WH_OPS_Credentials /user:SYSRPA /pass:ctfops2024
+
+cmdkey /generic:LJ_OPS_Credentials /user:SYSRPA /pass:ctfops2024
+
+
+cmdkey /generic:CTF_Credentials_WH /user:icc_rpa02 /pass:ctf@000000
+
+cmdkey /generic:CTF_Credentials_lj /user:icc_rpa02 /pass:ctf@000000
+
+cmdkey /generic:CTF_Credentials_SZ /user:icc_rpa02 /pass:ctf@000000
+
+
+cmdkey /generic:CTF_Credentials_WH_SCM /user:SYSRPA /pass:ctfcss2023
+
+cmdkey /generic:CTF_Credentials_lj_SCM /user:SYSRPA /pass:ctfcss2023
+
+cmdkey /generic:CTF_Credentials_SZ_SCM /user:SYSRPA /pass:ctfcss2023
